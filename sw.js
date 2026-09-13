072b24ae5b8bf4a274177bfcfa960550614ab8c5
David <salgadodavid2222@gmail.com>
2026-09-14 01:20:05 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'control-horas-v1';
+const CACHE_NAME = 'control-horas-v2';
 const CORE_FILES = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
